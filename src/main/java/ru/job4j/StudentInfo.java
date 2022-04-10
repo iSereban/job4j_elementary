06819803c79d429bd7910e4155d3166aa8bac375
iSereban <web-podderjka@mail.ru>
2022-04-10 20:52:30 +0300

    3. Арифметические операции. [#185]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey Koval");
+        System.out.println("11.04.1990");
+    }
+}
