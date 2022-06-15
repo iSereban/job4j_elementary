2490f0ea5c64911f7010a87aab5005fef66653b4
iSereban <web-podderjka@mail.ru>
2022-06-15 16:47:58 +0300

    6.4.2. Обход массива с последнего элемента

--- src/test/java/ru/job4j/array/RollBackArrayTest.java
+++ src/test/java/ru/job4j/array/RollBackArrayTest.java
@@ -7,32 +7,32 @@ import static org.junit.Assert.*;
 public class RollBackArrayTest {
     @Test
     public void whenEmpty() {
-        int[] input = new int[] {};
-        int[] expected = new int[] {};
+        int[] input = new int[]{};
+        int[] expected = new int[]{};
         int[] result = RollBackArray.rollback(input);
         assertArrayEquals(expected, result);
     }
 
     @Test
     public void whenOne() {
-        int[] input = new int[] {1};
-        int[] expected = new int[] {1};
+        int[] input = new int[]{1};
+        int[] expected = new int[]{1};
         int[] result = RollBackArray.rollback(input);
         assertArrayEquals(expected, result);
     }
 
     @Test
     public void whenFull() {
-        int[] input = new int[] {1, 2, 3, 4};
-        int[] expected = new int[] {4, 3, 2, 1};
+        int[] input = new int[]{1, 2, 3, 4};
+        int[] expected = new int[]{4, 3, 2, 1};
         int[] result = RollBackArray.rollback(input);
         assertArrayEquals(expected, result);
     }
 
     @Test
     public void whenTheSame() {
-        int[] input = new int[] {1, 1, 1, 1};
-        int[] expected = new int[] {1, 1, 1, 1};
+        int[] input = new int[]{1, 1, 1, 1};
+        int[] expected = new int[]{1, 1, 1, 1};
         int[] result = RollBackArray.rollback(input);
         assertArrayEquals(expected, result);
     }
